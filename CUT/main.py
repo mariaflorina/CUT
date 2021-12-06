5e5eb6d01684bcc77e57575f7bd4dc93a39531eb
Begu Maria Florina <maria.begu@info.uaic.ro>
2021-12-06 22:12:12 +0200

    CUT (-b,-c,-f,-d implemented)

--- CUT/main.py
+++ CUT/main.py
@@ -2,7 +2,7 @@ import re
 
 
 # verify that the command is valid
-# TODO: study other casses in which a command is not valid
+# TODO: study other cases in which a command is not valid
 def validate_command(command):
     options = 0
     if "-b" in command or "--bytes" in command:
@@ -19,8 +19,7 @@ def validate_command(command):
 # read command from console
 def read_command():
     print("Insert command:")
-    command = re.split(' |,', str(input()))
-    return command
+    return str(input())
 
 
 # read file with a given name
@@ -33,39 +32,88 @@ def read_file(file_name):
 
 
 # execute the command (only -b done)
-def process_command(command):
+def process_command(command, delim):
     validation = validate_command(command)
     if validation is True:
         print(command)
     files = [file for file in command if ".txt" in file]
-    # print(files)
+    print(files)
     options = [opt for opt in command if len(opt) > 1 and "-" == opt[0] and (opt[1].isdigit() is False)]
-    # print(options)
+    print(options)
     ranges = [ran for ran in command if ran not in files and any(map(str.isdigit, ran))]
-    # print(ranges)
-    bytes_command(command, files, options, ranges)  # -b option
-    # TODO: -c and -f
+    print(ranges)
+
+    if "-b" in options or "--bytes" in options:
+        bytes_characters_command(command, files, options, ranges)  # -b option
+    elif "-c" in options or "--characters" in options:
+        bytes_characters_command(command, files, options, ranges)  # -c option
+    elif "-f" in options or "--fields" in options:
+        fields_command(command, files, options, ranges, delim)  # -f option
+    # TODO: --complement and -s
 
 
-# read range for -b
-def read_range_bytes(command, lines, options, ranges):
+# read range for -b, -c
+def read_range_bytes_characters(command, lines, options, ranges):
     for line in lines:
         text = ""
         for r in ranges:
             if "-" in r:
-                a = int(r[0]) - 1
-                b = int(r[2])
+                a = 0
+                b = len(line)
+                if r[0].isdigit():
+                    a = int(r[0]) - 1
+                if len(r) > 2 and r[2].isdigit():
+                    b = int(r[2])
+                elif len(r) > 1 and r[1].isdigit():
+                    b = int(r[1])
                 text = text + line[a:b][:]
             else:
                 text = text + line[int(r[0]) - 1][:]
         print(text)
 
 
-# -b option
-def bytes_command(command, files, options, ranges):
+# -b, -c option
+def bytes_characters_command(command, files, options, ranges):
     for file in files:
         lines = read_file(file)
-        read_range_bytes(command, lines, options, ranges)
+        read_range_bytes_characters(command, lines, options, ranges)
 
 
-process_command(read_command())
+def read_delimiter_fields(command, lines, options, ranges, delim):
+    for line in lines:
+        line_delimited = line.split(delim)
+        text = ""
+        for r in ranges:
+            if "-" in r:
+                a = 0
+                b = len(line_delimited)
+                if r[0].isdigit():
+                    a = int(r[0]) - 1
+                if len(r) > 2 and r[2].isdigit():
+                    b = int(r[2])
+                elif len(r) > 1 and r[1].isdigit():
+                    b = int(r[1])
+                text = text + " ".join(line_delimited[a:b][:])
+            else:
+                text = text + " ".join(line_delimited[int(r[0]) - 1][:])
+        print(text)
+
+
+def fields_command(command, files, options, ranges, delim):
+    for file in files:
+        lines = read_file(file)
+        if "-d" not in options and "--delimiter" not in options:
+            print('\n'.join(lines))
+        else:
+            read_delimiter_fields(command, lines, options, ranges, delim)
+
+
+com = read_command()
+print(com)
+delim = ""
+if com.find("-d") or com.find("--delimiter"):
+    index = com.find("\"")
+    delim = com[index + 1]
+print(delim)
+command = re.split(' |,', com)
+process_command(command, delim)
